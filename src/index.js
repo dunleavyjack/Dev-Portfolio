b8c87a0833f3a196f65e1bfd0264cf8c68b0461d
dunleavyjack <dunleavyjack@gmail.com>
2021-07-12 00:48:24 +0900

    Added stylesheets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import './styles/stylesheet.css';
+import './styles/header.css';
 import AppRouter from './router/AppRouter';
 
 ReactDom.render(
