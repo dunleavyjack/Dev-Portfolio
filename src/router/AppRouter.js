280dedb29c40eabd75f7f7f6ed159bb47a69ebce
dunleavyjack <dunleavyjack@gmail.com>
2021-07-12 00:49:18 +0900

    Updated router

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Navbar from '../components/Navbar';
 import HomePage from '../pages/HomePage';
 import ProjectsPage from '../pages/ProjectsPage';
 import WritingPage from '../pages/WritingPage';
@@ -9,16 +10,19 @@ import ContactPage from '../pages/ContactPage';
 class AppRouter extends React.Component {
     render() {
         return (
-            <BrowserRouter>
-                <div className="main">
-                    <Switch>
-                        <Route path="/" component={HomePage} exact={true} />
-                        <Route path="/projects" component={ProjectsPage} />
-                        <Route path="/writing" component={WritingPage} />
-                        <Route path="/contact" component={ContactPage} />
-                    </Switch>
-                </div>
-            </BrowserRouter>
+            <>
+                <Navbar />
+                <BrowserRouter>
+                    <div className="main">
+                        <Switch>
+                            <Route path="/" component={HomePage} exact={true} />
+                            <Route path="/projects" component={ProjectsPage} />
+                            <Route path="/writing" component={WritingPage} />
+                            <Route path="/contact" component={ContactPage} />
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </>
         );
     }
 }
