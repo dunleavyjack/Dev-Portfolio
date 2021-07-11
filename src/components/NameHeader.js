6db4afcf6ecf8b2fb6001fed6e9003731a6ac192
dunleavyjack <dunleavyjack@gmail.com>
2021-07-12 00:48:32 +0900

    Added name header

---
+++ src/components/NameHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NameHeader = () => {
+    return (
+        <div className="header-container">
+            <h1 className="header">Jack Dunleavy</h1>
+        </div>
+    );
+};
+
+export default NameHeader;
