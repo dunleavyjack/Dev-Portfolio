a4c0c941307eb3a3b167ff56aab9c39ba1633cb6
dunleavyjack <dunleavyjack@gmail.com>
2021-07-13 02:10:38 +0900

    Added personal projects comp

---
+++ src/components/PersonalProjects.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import InfoHeader from './InfoHeader';
+import Project from './Project';
+import moodring from '../images/projectImages/moodringHR.png';
+import sclub from '../images/projectImages/sseuregiclubHR.png';
+import veryveryfar from '../images/projectImages/veryveryfar.png';
+
+const PersonalProjects = () => {
+    return (
+        <>
+            <InfoHeader text={'Personal Projects'} />
+            <Project
+                title={'Spotify Mood Ring'}
+                date={'March 2021'}
+                imgSrc={moodring}
+                alttext={'project 1'}
+            />
+            <Project
+                title={'Sseuregi Club'}
+                date={'February 2021'}
+                imgSrc={sclub}
+                alttext={'project 2'}
+            />
+            <Project
+                title={'Very Very Far'}
+                date={'April 2021'}
+                imgSrc={veryveryfar}
+                alttext={'project 3'}
+            />
+        </>
+    );
+};
+
+export default PersonalProjects;
