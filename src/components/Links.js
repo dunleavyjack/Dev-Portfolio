c1f6783b5555cabeae5ce210e811b5d0a9a85caa
dunleavyjack <dunleavyjack@gmail.com>
2021-07-13 02:10:14 +0900

    Added links comp

---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Links = () => {
+    return (
+        <div className="links-container">
+            <a href="https://github.com/dunleavyjack">GITHUB</a>
+            <a href="https://jackdunleavy.medium.com">MEDIUM</a>
+            <a href="https://www.linkedin.com/in/jack-dunleavy-0b177b83/">
+                LINKEDIN
+            </a>
+        </div>
+    );
+};
+
+export default Links;
