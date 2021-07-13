9e96f44e140f8bb08355fd21068abab5ece36765
dunleavyjack <dunleavyjack@gmail.com>
2021-07-13 23:22:45 +0900

    Moved around title header

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Project = ({ title, date, imgSrc, altText }) => {
+const Project = ({ title, date, description, imgSrc, altText }) => {
     return (
         <div>
             <h1 className="project-text">
                 {title} | {date}
             </h1>
+            <p className={'project-description'}>{description}</p>
             <img className="project-image" src={imgSrc} alt={altText}></img>
         </div>
     );
