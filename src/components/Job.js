2e862799d9d5ca7839256d1e5248a966c2ad0b87
dunleavyjack <dunleavyjack@gmail.com>
2021-07-13 23:22:21 +0900

    Tweaked job component

--- src/components/Job.js
+++ src/components/Job.js
@@ -3,18 +3,12 @@ import React from 'react';
 const Job = ({ title, description, duration, company, link }) => {
     return (
         <div className="job-container">
-            <p className="job-description">
-                {duration} | {description}
-            </p>
             <a className="job-link" href={link}>
-                <p className="job-title">
-                    {title.toUpperCase()}
-                    <span className="job-company">
-                        {' - '}
-                        {company}
-                    </span>
-                </p>
+                <p className="job-title">{title.toUpperCase()}</p>
             </a>
+            <p className="job-description">
+                {company} | {description} | {duration}
+            </p>
         </div>
     );
 };
