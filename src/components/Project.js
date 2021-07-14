f29a54aba328bd03a668d3d50fded146f60cc47e
dunleavyjack <dunleavyjack@gmail.com>
2021-07-14 17:39:35 +0900

    Adding hover text

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,7 +7,10 @@ const Project = ({ title, date, description, imgSrc, altText }) => {
                 {title} | {date}
             </h1>
             <p className={'project-description'}>{description}</p>
-            <img className="project-image" src={imgSrc} alt={altText}></img>
+            <div className={'project-image-container'}>
+                <img className="project-image" src={imgSrc} alt={altText}></img>
+                <div className={'project-image-text'}>SEE MORE</div>
+            </div>
         </div>
     );
 };
