4a3a4c127df1a2a60bb1728950c52623539eeb83
dunleavyjack <dunleavyjack@gmail.com>
2021-07-17 14:21:46 +0900

    Changed links to page links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,22 +2,13 @@ import React from 'react';
 import Chevron from '../images/chevron-arrow-down.png';
 
 const Navbar = () => {
-    // const myFunction = () => {
-    //     const x = document.getElementById('customNav');
-    //     if (x.className === 'navbar') {
-    //         x.className += ' responsive';
-    //     } else {
-    //         x.className = 'navbar';
-    //     }
-    // };
-
     return (
         <div className="navbar" id="customNav">
             <a href="/" className="active">
                 HELLO
             </a>
-            <a href="/projects">PROJECTS</a>
-            <a href="/writing">WRITING</a>
+            <a href="#projects">PROJECTS</a>
+            <a href="#writing">WRITING</a>
             <a href="/contact">CONTACT</a>
             <img className="chevron-down" src={Chevron} alt="Down Arrow"></img>
         </div>
