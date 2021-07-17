b031aed27e61a6dcf70591e249da25bd42a03ae2
dunleavyjack <dunleavyjack@gmail.com>
2021-07-17 14:23:24 +0900

    Removed experience from home page

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Project = ({ title, date, description, imgSrc, altText }) => {
     return (
         <div>
-            <h1 className="project-text">
-                {title} | {date}
-            </h1>
-            <p className={'project-description'}>{description}</p>
             <div className={'project-image-container'}>
                 <img className="project-image" src={imgSrc} alt={altText}></img>
-                <div className={'project-image-text'}>SEE MORE</div>
+                <div className={'project-image-text'}>
+                    <h1 className="project-text">
+                        {title} | {date}
+                    </h1>
+                </div>
             </div>
         </div>
     );
