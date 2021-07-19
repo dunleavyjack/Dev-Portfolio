a79abe04d12dbe1703d88aa0ddcbc0dbfd240036
dunleavyjack <dunleavyjack@gmail.com>
2021-07-19 23:53:27 +0900

    Reorganized

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,12 @@ import React from 'react';
 import NameHeader from '../components/NameHeader';
 import Overview from '../components/Overview';
 import PersonalProjects from '../components/PersonalProjects';
+import AnimatedNameHeader from '../components/AnimatedNameHeader';
 
 const HomePage = () => {
     return (
         <div className="page-container">
-            <NameHeader />
+            <AnimatedNameHeader />
             <Overview />
             <PersonalProjects />
         </div>
