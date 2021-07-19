4eed8a815f050d3c84cb504e5b987768be9d296c
dunleavyjack <dunleavyjack@gmail.com>
2021-07-19 23:53:22 +0900

    Reorganized

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 
-const Project = ({ title, date, description, imgSrc, altText }) => {
+const Project = ({ title, date, description, imgSrc, altText, history }) => {
     return (
-        <div>
-            <div className={'project-image-container'}>
-                <img className="project-image" src={imgSrc} alt={altText}></img>
-                <div className={'project-image-text'}>
-                    <h1 className="project-text">
-                        {title} | {date}
-                    </h1>
-                </div>
+        <div
+            className={'project-image-container'}
+            onClick={() => console.log('clicked')}
+            // onClick={() => history.push('/projects/sseuregiclub')}
+        >
+            <img className="project-image" src={imgSrc} alt={altText}></img>
+            <div className={'project-image-text'}>
+                <h1 className="project-text">
+                    {title} | {date}
+                </h1>
             </div>
         </div>
     );
 };
 
-export default Project;
+export default withRouter(Project);
