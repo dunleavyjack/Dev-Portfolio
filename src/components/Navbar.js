c65b433fa1b595cb4303e4b2afab342bc76e1c61
dunleavyjack <dunleavyjack@gmail.com>
2021-07-24 15:03:01 +0900

    Updated nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import Chevron from '../images/chevron-arrow-down.png';
+import React, { useState } from 'react';
+import ChevronDown from '../images/chevron-arrow-down.png';
+import ChevronUp from '../images/chevron-arrow-up.png';
+import github from '../assets/images/github.png';
+import logo from '../assets/images/namelogo.png';
+import burger from '../images/burger.png';
+import dots from '../images/dots.png';
+import DropUp from '../components/DropUp';
 
 const Navbar = () => {
+    const [atTop, setAtTop] = useState(true);
+
+    document.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            setAtTop(false);
+        } else {
+            setAtTop(true);
+        }
+    });
+
+    const toggleActive = (page) => {
+        return window.location.pathname === page ? 'active' : '';
+    };
+
     return (
-        <div className="navbar" id="customNav">
-            <a href="/" className="active">
-                HELLO
-            </a>
-            <a href="#projects">PROJECTS</a>
-            <a href="#writing">WRITING</a>
-            <a href="/contact">CONTACT</a>
-            <img className="chevron-down" src={Chevron} alt="Down Arrow"></img>
-        </div>
+        <>
+            <nav className={`navbar ${atTop ? null : 'floating'}`}>
+                <div className="logo">
+                    <a href="/">
+                        <img
+                            src={logo}
+                            alt="Name Logo"
+                            height={'50px'}
+                            width={'83px'}
+                        ></img>
+                    </a>
+                </div>
+                <div className="navlinks" id="customNav">
+                    <a href="/" className={toggleActive('/')}>
+                        HELLO
+                    </a>
+                    <a href="/projects" className={toggleActive('/projects')}>
+                        PROJECTS
+                    </a>
+                    <a
+                        href="/experience"
+                        className={toggleActive('/experience')}
+                    >
+                        EXPERIENCE
+                    </a>
+                    <a href="/contact" className={toggleActive('/contact')}>
+                        CONTACT
+                    </a>
+                </div>
+                <div className="github">
+                    <a href="https://github.com/dunleavyjack">
+                        <img
+                            src={github}
+                            alt="Github Link Icon"
+                            height={30}
+                            width={30}
+                        />
+                    </a>
+                </div>
+                <div className="burger">
+                    <img src={dots}></img>
+                </div>
+            </nav>
+        </>
     );
 };
 
