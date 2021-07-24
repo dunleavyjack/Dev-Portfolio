72b3c059870ab492b3bb870986eabee85f05527d
dunleavyjack <dunleavyjack@gmail.com>
2021-07-24 15:03:27 +0900

    Added component

---
+++ src/components/SeeMore.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SeeMore = () => {
+    return (
+        <div className="center">
+            <a className="page-link" href="/projects">
+                SEE MORE
+            </a>
+        </div>
+    );
+};
+
+export default SeeMore;
