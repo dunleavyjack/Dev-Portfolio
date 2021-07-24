1e7127e52e41767406e25f04d9235e0807d15d68
dunleavyjack <dunleavyjack@gmail.com>
2021-07-24 15:03:43 +0900

    Added exerperience page

---
+++ src/pages/ExperiencePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExperiencePage = () => {
+    return (
+        <div>
+            <h1>Experience Page</h1>
+            <h1>Experience Page</h1>
+        </div>
+    );
+};
+
+export default ExperiencePage;
