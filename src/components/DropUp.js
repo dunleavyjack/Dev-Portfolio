b93981dc4832d32994e4868f2bfeb001cfb325bc
dunleavyjack <dunleavyjack@gmail.com>
2021-07-24 15:02:35 +0900

    Added dropup

---
+++ src/components/DropUp.js
@@ -0,0 +1,11 @@
+const DropUp = () => {
+    return (
+        <div className="drop-links">
+            <a href="/" className="active">
+                HELLO
+            </a>
+        </div>
+    );
+};
+
+export default DropUp;
