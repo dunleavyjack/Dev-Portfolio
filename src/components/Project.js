dae5ba99e4a279350c8247dd94275314db832ff8
dunleavyjack <dunleavyjack@gmail.com>
2021-07-24 15:03:19 +0900

    Created project cmponent

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 
-const Project = ({ title, date, description, imgSrc, altText, history }) => {
+const Project = ({ title, date, imgSrc, altText, history }) => {
     return (
-        <div
-            className={'project-image-container'}
-            onClick={() => console.log('clicked')}
-            // onClick={() => history.push('/projects/sseuregiclub')}
-        >
-            <img className="project-image" src={imgSrc} alt={altText}></img>
-            <div className={'project-image-text'}>
-                <h1 className="project-text">
-                    {title} | {date}
-                </h1>
+        <div className={'project-image-container'}>
+            <div className="big-project-link">
+                <img className="project-image" src={imgSrc} alt={altText}></img>
+                <div className={'project-image-text'}>
+                    <h1 className="project-text">
+                        {title} | {date}
+                    </h1>
+                </div>
             </div>
         </div>
     );
