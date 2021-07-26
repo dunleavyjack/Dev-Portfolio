1458165668eddbb2de5aec10e4d201c8ddc97a6e
dunleavyjack <dunleavyjack@gmail.com>
2021-07-26 23:50:22 +0900

    Updated

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-const Links = () => {
-    return (
-        <div className="links-container">
-            <a href="https://github.com/dunleavyjack">GITHUB</a>
-            <a href="https://jackdunleavy.medium.com">MEDIUM</a>
-            <a href="https://www.linkedin.com/in/jack-dunleavy-0b177b83/">
-                LINKEDIN
-            </a>
-        </div>
-    );
+const Links = ({ data }) => {
+    const links = data.map((link) => {
+        return <a href={link.ref}>{link.label}</a>;
+    });
+
+    return <div className="links-container">{links}</div>;
 };
 
 export default Links;
