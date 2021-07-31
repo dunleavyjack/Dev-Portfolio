97658e2f043799869a4d17c3b7325db995d064ef
dunleavyjack <dunleavyjack@gmail.com>
2021-07-31 23:57:50 +0900

    Updated

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,29 +1,73 @@
 import React from 'react';
+import github from '../assets/images/github.png';
 import {
     Drawer as MUIDrawer,
     List,
     ListItem,
-    ListItemIcon,
     ListItemText,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Drawer = ({ isOpen }) => {
+const useStyles = makeStyles({
+    drawer: {
+        width: '800px',
+    },
+});
+
+const drawerLinks = [
+    {
+        name: 'HELLO',
+        link: '/',
+    },
+    {
+        name: 'PROJECTS',
+        link: '/projects',
+    },
+    {
+        name: 'EXPERIENCE',
+        link: '/experience',
+    },
+    {
+        name: 'CONTACT',
+        link: '/contact',
+    },
+];
+
+const drawerItems = drawerLinks.map(({ link, name }, index) => {
     return (
-        <MUIDrawer open={isOpen} varient="temporary">
-            <List>
-                {
-                    <List>
-                        {['HELLO', 'PROJECTS', 'EXPERIENCE', 'CONTACT'].map(
-                            (text, index) => (
-                                <ListItem button key={text}>
-                                    <ListItemText primary={text} />
-                                </ListItem>
-                            )
-                        )}
-                    </List>
-                }
-            </List>
-        </MUIDrawer>
+        <ListItem key={index}>
+            <a href={link} className="drawer-links">
+                <ListItemText primary={name} disableTypography={true} />
+            </a>
+        </ListItem>
+    );
+});
+
+const Drawer = ({ isOpen, toggleDrawer }) => {
+    const classes = useStyles();
+    return (
+        <div onClick={() => toggleDrawer()}>
+            <MUIDrawer
+                open={isOpen}
+                onClose={true}
+                varient="temporary"
+                className={classes.drawer}
+            >
+                <div className="github-drawer-container">
+                    <a href="https://github.com/dunleavyjack">
+                        <img
+                            src={github}
+                            alt="Github Link Icon"
+                            height={40}
+                            width={40}
+                        />
+                    </a>
+                </div>
+                <List>
+                    <List>{drawerItems}</List>
+                </List>
+            </MUIDrawer>
+        </div>
     );
 };
 
