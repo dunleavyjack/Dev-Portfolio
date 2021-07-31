8020ee1c70b5517c7788ba982f99ac54f748995b
dunleavyjack <dunleavyjack@gmail.com>
2021-07-31 23:58:09 +0900

    Updated

---
+++ src/components/ProjectPage/ProjectPageTitle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProjectPageHeader from './ProjectPageHeader';
+
+const ProjectPageTitle = () => {
+    return (
+        <div className="title-container">
+            <h1 style={{ display: 'block' }}>PROJECTS</h1>
+            <ProjectPageHeader />
+        </div>
+    );
+};
+
+export default ProjectPageTitle;
