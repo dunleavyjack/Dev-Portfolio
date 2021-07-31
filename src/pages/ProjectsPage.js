d0ccb07b2fc67906f394c751e984621087c02eb1
dunleavyjack <dunleavyjack@gmail.com>
2021-07-31 23:58:24 +0900

    Updated

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import NavSpacer from '../components/NavSpacer';
-import AllProjects from '../components/ProductPage/AllProjects';
-import ProductPageHeader from '../components/ProductPage/ProductPageHeader';
+import AllProjects from '../components/ProjectPage/AllProjects';
+import ProjectPageTitle from '../components/ProjectPage/ProjectPageTitle';
 
 const ProjectsPage = () => {
     return (
         <div className="page-container">
             <div className="page">
                 <NavSpacer />
-                <h1>Projects Page</h1>
-                <ProductPageHeader />
+                <ProjectPageTitle />
+                {/* <ProductPageHeader /> */}
                 <AllProjects />
             </div>
         </div>
