f31dc9f9cfd6db5cd79087a2b0032076065ffe8a
dunleavyjack <dunleavyjack@gmail.com>
2021-08-03 23:06:35 +0900

    Added images

--- src/components/PersonalProjects.js
+++ src/components/PersonalProjects.js
@@ -8,27 +8,27 @@ import veryveryfar from '../images/projectImages/veryveryfar.png';
 const PersonalProjects = () => {
     return (
         <>
-            <InfoHeader text={'Recent Projects'} link={'projects'} />
+            <InfoHeader text={'Featured Projects'} link={'projects'} />
             <Project
                 title={'Sseuregi Club'}
                 date={'April 2021'}
                 imgSrc={sclub}
-                alttext={'project 2'}
+                altText={'project 2'}
+                link="sseuregiclub"
             />
             <Project
                 title={'Spotify Mood Ring'}
                 date={'March 2021'}
-                description={
-                    "A mobile-first React web app that determines a user's mood based on their recently played music on Spotify."
-                }
                 imgSrc={moodring}
-                alttext={'project 1'}
+                altText={'project 1'}
+                link={'moodring'}
             />
             <Project
                 title={'Very Very Far'}
                 date={'February 2021'}
                 imgSrc={veryveryfar}
-                alttext={'project 3'}
+                altText={'project 3'}
+                link={'veryveryfar'}
             />
         </>
     );
