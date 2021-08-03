d8782640e6d6cf790f20633e1ff36e32b9659925
dunleavyjack <dunleavyjack@gmail.com>
2021-08-03 23:07:19 +0900

    updated

--- src/components/ProjectPage/IndividualProjects/StaticProjectAbout.js
+++ src/components/ProjectPage/IndividualProjects/StaticProjectAbout.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const StaticProjectAbout = ({ text }) => {
     return (
-        <div>
+        <>
             <h1 className="project-about">ABOUT</h1>
             {text}
-        </div>
+        </>
     );
 };
 
