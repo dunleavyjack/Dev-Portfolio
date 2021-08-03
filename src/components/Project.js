d607bf8242738ee540c31579f8890b787f9ffbbc
dunleavyjack <dunleavyjack@gmail.com>
2021-08-03 23:06:52 +0900

    updated

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,32 @@
 import React from 'react';
 
-const Project = ({ title, date, imgSrc, altText, history }) => {
+const Project = ({ title, date, imgSrc, altText, link }) => {
+    const handleClick = () => {
+        window.location = link;
+    };
+
     return (
-        <div className={'project-image-container'}>
-            <img className="project-image" src={imgSrc} alt={altText}></img>
-            <div className={'project-image-text'}>
-                <h1 className="project-text">
-                    {title} | {date}
-                </h1>
+        <>
+            <h1 className="project-text-mobile">
+                {title} | {date}
+            </h1>
+            <div className={'project-image-container'}>
+                <img
+                    className="project-image"
+                    src={imgSrc}
+                    alt={altText}
+                    onClick={() => handleClick()}
+                ></img>
+                <div
+                    className={'project-image-text'}
+                    onClick={() => handleClick()}
+                >
+                    <h1 className="project-text">
+                        {title} | {date}
+                    </h1>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
