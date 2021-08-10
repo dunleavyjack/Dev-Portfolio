9c73350b5970e771ca230d9ceb2bf316a14136b6
dunleavyjack <dunleavyjack@gmail.com>
2021-08-10 14:29:14 +0900

    Optamized speed

--- src/pages/Hello/components/AnimatedNameHeader.js
+++ src/pages/Hello/components/AnimatedNameHeader.js
@@ -1,68 +1,132 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const AnimatedNameHeader = () => {
-    let leon, canvas, ctx;
+    // let leon, canvas, ctx;
 
-    const sw = 800;
-    const sh = 400;
-    const pixelRatio = 2;
+    // const sw = 800;
+    // const sh = 400;
+    // const pixelRatio = 2;
 
-    const init = () => {
-        canvas = document.createElement('canvas');
-        const referenceNode =
-            document.getElementsByClassName('reference-node')[0];
-        const parentNode = document.getElementsByClassName('parent-node')[0];
+    // const init = () => {
+    //     canvas = document.createElement('canvas');
+    //     const referenceNode =
+    //         document.getElementsByClassName('reference-node')[0];
+    //     const parentNode = document.getElementsByClassName('parent-node')[0];
 
-        parentNode.insertBefore(canvas, referenceNode);
-        ctx = canvas.getContext('2d');
-        canvas.width = sw * pixelRatio;
-        canvas.height = sh * pixelRatio;
-        canvas.style.width = '100%';
-        canvas.style.height = sh;
-        ctx.scale(pixelRatio, pixelRatio);
+    //     parentNode.insertBefore(canvas, referenceNode);
+    //     ctx = canvas.getContext('2d');
+    //     canvas.width = sw * pixelRatio;
+    //     canvas.height = sh * pixelRatio;
+    //     canvas.style.width = '100%';
+    //     canvas.style.height = sh;
+    //     ctx.scale(pixelRatio, pixelRatio);
 
-        leon = new window.LeonSans({
-            text: 'Jack Dunleavy',
-            color: ['#000000'],
-            size: 80,
-            weight: 200,
-        });
+    //     leon = new window.LeonSans({
+    //         text: 'Jack Dunleavy',
+    //         color: ['#000000'],
+    //         size: 80,
+    //         weight: 200,
+    //     });
 
-        requestAnimationFrame(animate);
+    //     requestAnimationFrame(animate);
 
-        let i,
-            total = leon.drawing.length;
-        for (i = 0; i < total; i++) {
-            window.TweenMax.fromTo(
-                leon.drawing[i],
-                1.6,
-                {
-                    value: 0,
-                },
-                {
-                    delay: i * 0.05,
-                    value: 1,
-                    ease: window.Power4.easeOut,
-                }
-            );
-        }
-    };
+    //     let i,
+    //         total = leon.drawing.length;
+    //     for (i = 0; i < total; i++) {
+    //         window.TweenMax.fromTo(
+    //             leon.drawing[i],
+    //             1.6,
+    //             {
+    //                 value: 0,
+    //             },
+    //             {
+    //                 delay: i * 0.05,
+    //                 value: 1,
+    //                 ease: window.Power4.easeOut,
+    //             }
+    //         );
+    //     }
+    // };
 
-    const animate = (t) => {
-        requestAnimationFrame(animate);
+    // const animate = (t) => {
+    //     requestAnimationFrame(animate);
 
-        ctx.clearRect(0, 0, sw, sh);
+    //     ctx.clearRect(0, 0, sw, sh);
 
-        const x = (sw - leon.rect.w) / 2;
-        const y = (sh - leon.rect.h) / 2;
-        leon.position(x, y);
+    //     const x = (sw - leon.rect.w) / 2;
+    //     const y = (sh - leon.rect.h) / 2;
+    //     leon.position(x, y);
 
-        leon.draw(ctx);
-    };
+    //     leon.draw(ctx);
+    // };
 
-    window.onload = () => {
+    // window.onload = () => {
+    //     init();
+    // };
+
+    useEffect(() => {
+        let leon, canvas, ctx;
+
+        const sw = 800;
+        const sh = 400;
+        const pixelRatio = 2;
+
+        const init = () => {
+            canvas = document.createElement('canvas');
+            const referenceNode =
+                document.getElementsByClassName('reference-node')[0];
+            const parentNode =
+                document.getElementsByClassName('parent-node')[0];
+
+            parentNode.insertBefore(canvas, referenceNode);
+            ctx = canvas.getContext('2d');
+            canvas.width = sw * pixelRatio;
+            canvas.height = sh * pixelRatio;
+            canvas.style.width = '100%';
+            canvas.style.height = sh;
+            ctx.scale(pixelRatio, pixelRatio);
+
+            leon = new window.LeonSans({
+                text: 'Jack Dunleavy',
+                color: ['#000000'],
+                size: 80,
+                weight: 200,
+            });
+
+            requestAnimationFrame(animate);
+
+            let i,
+                total = leon.drawing.length;
+            for (i = 0; i < total; i++) {
+                window.TweenMax.fromTo(
+                    leon.drawing[i],
+                    1.6,
+                    {
+                        value: 0,
+                    },
+                    {
+                        delay: i * 0.05,
+                        value: 1,
+                        ease: window.Power4.easeOut,
+                    }
+                );
+            }
+        };
+
+        const animate = (t) => {
+            requestAnimationFrame(animate);
+
+            ctx.clearRect(0, 0, sw, sh);
+
+            const x = (sw - leon.rect.w) / 2;
+            const y = (sh - leon.rect.h) / 2;
+            leon.position(x, y);
+
+            leon.draw(ctx);
+        };
+
         init();
-    };
+    }, []);
 
     return (
         <div id="top" className="parent-node">
