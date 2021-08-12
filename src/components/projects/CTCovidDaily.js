dbf38939e04375ba26a82b19a37c589df71c0bc3
dunleavyjack <dunleavyjack@gmail.com>
2021-08-12 09:45:48 +0900

    Added text

--- src/components/projects/CTCovidDaily.js
+++ src/components/projects/CTCovidDaily.js
@@ -3,6 +3,7 @@ import IndividualProjectHeader from '../../pages/ProjectDisplay/components/Indiv
 import NavSpacer from '../NavSpacer';
 import StaticProjectImage from '../../pages/ProjectDisplay/components/IndividualProjects/StaticProjectImage';
 import ctcovid from '../../images/projectImages/CovidCT/ctcovid-mockup-1.png';
+import TextSpacer from '../TextSpacer';
 
 const CTCovidDaily = () => {
     return (
@@ -31,11 +32,25 @@ const CTCovidDaily = () => {
                     </span>
                     <br />
                     <br />
-                    Like a lot of people, when the pandemic firrst hit I spent a
-                    lot of time checking the current cases and rate of tests for
-                    my area.
+                    Like a lot of people, when the pandemic first hit I spent a
+                    lot of time checking the current cases for my area.
+                    Routinely, I would look at the number of new cases, the
+                    amount of tests being conducted, and the rising total cases.
                 </p>
                 <StaticProjectImage image={ctcovid} />
+
+                <p className="project-description">
+                    Because I was checking so often, the process of opening the
+                    government website and navigating to the daily cases was a
+                    bit time consuming. I wondered if I could find a more
+                    convienent way to display the information and I decided to
+                    make an iOS application. I no longer live in Connecticut,
+                    but my parents do and when checking the COVID numbers there
+                    I noticed the local governement had an free, open API for
+                    COVID data. I figured that would be a good place to start
+                </p>
+
+                <TextSpacer />
                 <h1 className="project-about">The Tech 👨🏼‍💻</h1>
                 <ul className="project-tech">
                     <li>
